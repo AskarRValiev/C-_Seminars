1618c36cebc9addeefbc370ef4e255debc59203c
Askar <oskarelectrotrans@gmail.com>
2022-04-12 00:52:54 +0300

    Homework from 1st seminar

--- HomeWorks_1/Program.cs
+++ HomeWorks_1/Program.cs
@@ -45,7 +45,7 @@ else
 */
 
 //Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-/*
+
 Console.WriteLine("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -53,7 +53,6 @@ int i = 1;
 while (i <= num)
 {
     if (i % 2 == 0)
-        Console.Write(i + " ");
+        Console.Write(i + ", ");
         i++;
 }
-*/
